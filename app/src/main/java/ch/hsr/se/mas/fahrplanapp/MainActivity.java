76275d0420f0b4f5d740be40b8659087b2ffe777
Igor Gretener <igor.gretener@hsr.ch>
2017-01-30 20:20:18 +0100

    Prepared main activity to react on search button

--- app/src/main/java/ch/hsr/se/mas/fahrplanapp/MainActivity.java
+++ app/src/main/java/ch/hsr/se/mas/fahrplanapp/MainActivity.java
@@ -1,10 +1,9 @@
 package ch.hsr.se.mas.fahrplanapp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,7 +14,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener{
+        implements NavigationView.OnNavigationItemSelectedListener,
+        ConnectionSearchFragment.ConnectionSearchFragmentInteractionListener,
+        ConnectionOverviewFragment.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,14 +34,6 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
-        Button btn = (Button) findViewById(R.id.button_search);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                LoadConnections();
-            }
-        });
     }
 
     @Override
@@ -55,19 +48,14 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
@@ -77,12 +65,11 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
 
         switch (id){
             case R.id.directions:
-                Intent intent = new Intent(this, AboutActivity.class);
+                Intent intent = new Intent(this, MainActivity.class);
                 startActivity(intent);
                 break;
             case R.id.about:
@@ -98,7 +85,13 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
-    private void LoadConnections() {
+    @Override
+    public void onSearchStarted() {
         new SearchConnectionsAsyncTask().execute();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
