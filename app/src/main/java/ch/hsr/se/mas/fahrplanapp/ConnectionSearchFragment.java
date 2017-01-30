0f2aa5b7be4ac02ad3004d339e98adc1d66065d2
Igor Gretener <igor.gretener@hsr.ch>
2017-01-30 21:50:43 +0100

    UI implented for the connection search

--- app/src/main/java/ch/hsr/se/mas/fahrplanapp/ConnectionSearchFragment.java
+++ app/src/main/java/ch/hsr/se/mas/fahrplanapp/ConnectionSearchFragment.java
@@ -1,46 +1,129 @@
 package ch.hsr.se.mas.fahrplanapp;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
 
-import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 
 public class ConnectionSearchFragment extends Fragment {
-    private Date searchDate;
+    private Calendar searchDate;
+    private boolean departureSearch;
+    private DatePickerDialog connectionDatePickerDialog;
+    private TimePickerDialog connectionTimePickerDialog;
+    private SimpleDateFormat dateFormatter;
+    private SimpleDateFormat timeFormatter;
+
+    Button btnSearch;
+    Button btnTimeReferenceSelection;
+    Button btnDatePicker;
+    Button btnTimePicker;
 
     private ConnectionSearchFragmentInteractionListener mListener;
 
     public ConnectionSearchFragment() {
-        searchDate = new Date(System.currentTimeMillis());
+        searchDate = Calendar.getInstance();
+        departureSearch = true;
     }
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        dateFormatter = new SimpleDateFormat(getString(R.string.date_format), Locale.getDefault());
+        timeFormatter = new SimpleDateFormat(getString(R.string.time_format), Locale.getDefault());
+
+        connectionDatePickerDialog = new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                searchDate.set(Calendar.YEAR, year);
+                searchDate.set(Calendar.MONTH, monthOfYear);
+                searchDate.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                btnDatePicker.setText(dateFormatter.format(searchDate.getTime()));
+            }
+        },searchDate.get(Calendar.YEAR), searchDate.get(Calendar.MONTH), searchDate.get(Calendar.DAY_OF_MONTH));
+
+        connectionTimePickerDialog = new TimePickerDialog(getContext(), new TimePickerDialog.OnTimeSetListener() {
+
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                searchDate.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                searchDate.set(Calendar.MINUTE, minute);
+                btnTimePicker.setText(timeFormatter.format(searchDate.getTime()));
+            }
+        }, searchDate.get(Calendar.HOUR_OF_DAY), searchDate.get(Calendar.MINUTE), true);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_connection_search, container, false);
 
-        Button btn = (Button) view.findViewById(R.id.button_search);
-        btn.setOnClickListener(new View.OnClickListener() {
+        btnSearch = (Button) view.findViewById(R.id.button_search);
+        btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onSearchButtonPressed();
             }
         });
 
+        btnTimeReferenceSelection = (Button) view.findViewById(R.id.button_time_reference);
+        btnTimeReferenceSelection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onTimeReferenceButtonPressed();
+            }
+        });
+
+        btnDatePicker = (Button) view.findViewById(R.id.button_date_picker);
+        btnDatePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onDatePickerButtonPressed();
+            }
+        });
+        btnDatePicker.setText(dateFormatter.format(searchDate.getTime()));
+
+        btnTimePicker = (Button) view.findViewById(R.id.button_time_picker);
+        btnTimePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onTimePickerButtonPressed();
+            }
+        });
+        btnTimePicker.setText(timeFormatter.format(searchDate.getTime()));
+
         return view;
+    }
+
+    public void onTimeReferenceButtonPressed() {
+        // Swap reference from departure to arrival and vice versa
+        departureSearch = !departureSearch;
+        if (departureSearch) {
+            btnTimeReferenceSelection.setText(getString(R.string.departure_search));
+        } else {
+            btnTimeReferenceSelection.setText(getString(R.string.arrival_search));
+        }
+    }
+
+    public void onDatePickerButtonPressed() {
+        connectionDatePickerDialog.show();
+    }
+
+    public void onTimePickerButtonPressed() {
+        connectionTimePickerDialog.show();
     }
 
     public void onSearchButtonPressed() {
