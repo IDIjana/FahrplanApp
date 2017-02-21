3b1de27fcb47ca6aa7ef34ae17f9a1415284b0ba
Hermann Bossi <osiris-888@hotmail.com>
2017-02-21 15:32:18 +0100

    Some small updates on Settings

--- app/src/main/java/ch/hsr/se/mas/fahrplanapp/SettingsFragment.java
+++ app/src/main/java/ch/hsr/se/mas/fahrplanapp/SettingsFragment.java
@@ -8,7 +8,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.Button;
 
 /**
  * Created by Hermann on 21.02.2017.
@@ -16,9 +18,12 @@ import android.widget.Switch;
 
 public class SettingsFragment extends Fragment {
 
+        Settings set1;
+        View view;
+
         public View onCreateSettingsView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) throws IOException, ClassNotFoundException {
-            View view = inflater.inflate(R.layout.fragment_settings, container, false);
-            Settings set1 = new Settings();
+            view = inflater.inflate(R.layout.fragment_settings, container, false);
+            set1 = new Settings();
 
                 CheckBox checkTrain = (CheckBox) view.findViewById(R.id.cbxtrain);
                 checkTrain.setChecked(set1.containstransp(Transportation.TRAIN));
@@ -32,10 +37,56 @@ public class SettingsFragment extends Fragment {
                 checkBus.setChecked(set1.containstransp(Transportation.SHIP));
                 Switch classSwitch = (Switch) view.findViewById(R.id.selectclass);
                 classSwitch.setChecked(set1.getClasse()==1);
-
-
+                EditText homeStation = (EditText) view.findViewById(R.id.takemehome);
+                homeStation.setText(set1.getHome());
+                Button btn = (Button) view.findViewById(R.id.buttonsaveSettings);
+                btn.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    EditText homeStation = (EditText) view.findViewById(R.id.takemehome);
+                    set1.setHome(homeStation.getText().toString());
+                    set1.serializeSettings();
+                }
+            });
             return view;
         }
+
+    public void onCheckboxClicked(View view) {
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
+
+        // Check which checkbox was clicked
+        switch(view.getId()) {
+            case R.id.cbxtrain:
+                if (checked) set1.addTransportation(Transportation.TRAIN);
+                else set1.removeTransportation(Transportation.TRAIN);
+                break;
+            case R.id.cbxbus:
+                if (checked) set1.addTransportation(Transportation.BUS);
+                else set1.removeTransportation(Transportation.BUS);
+                break;
+            case R.id.cbxtram:
+                if (checked) set1.addTransportation(Transportation.TRAM);
+                else set1.removeTransportation(Transportation.TRAM);
+                break;
+            case R.id.cbxship:
+                if (checked) set1.addTransportation(Transportation.SHIP);
+                else set1.removeTransportation(Transportation.SHIP);
+                break;
+            case R.id.cbxcablecar:
+                if (checked) set1.addTransportation(Transportation.CABLECAR);
+                else set1.removeTransportation(Transportation.CABLECAR);
+                break;
+        }
+    }
+
+    public void onSwitchClicked(View view){
+        boolean checked = ((Switch) view).isChecked();
+
+        if(checked) set1.setClasse(1);
+        else set1.setClasse(2);
+
+    }
+
 
 
 }
