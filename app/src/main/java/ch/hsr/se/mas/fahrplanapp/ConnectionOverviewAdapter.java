0dc7827927c58045576322651b6c92eecea0c767
Alexandra Zeiler <alexandrazeiler@hotmail.com>
2017-02-22 18:28:56 +0100

    Connection Overview angepasst. Weitere Infos hinzugefügt: Auslastung pro Klasse, Verspätungen, erstes Verkehrsmittel (keine Doku zu CategoryCode gefunen, Bus und Zug sollte aber stimmen..)

--- app/src/main/java/ch/hsr/se/mas/fahrplanapp/ConnectionOverviewAdapter.java
+++ app/src/main/java/ch/hsr/se/mas/fahrplanapp/ConnectionOverviewAdapter.java
@@ -1,10 +1,12 @@
 package ch.hsr.se.mas.fahrplanapp;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
@@ -21,10 +23,11 @@ public class ConnectionOverviewAdapter extends BaseAdapter {
     private Context context;
     private List<Connection> connections;
 
-    public ConnectionOverviewAdapter(Context context,ConnectionList connections) {
+    public ConnectionOverviewAdapter(Context context, ConnectionList connections) {
         this.context = context;
         this.connections = connections.getConnections();
     }
+
     @Override
     public int getCount() {
         return connections.size();
@@ -48,27 +51,122 @@ public class ConnectionOverviewAdapter extends BaseAdapter {
         }
 
         SimpleDateFormat tf = new SimpleDateFormat("HH:mm", Locale.getDefault());
-        Connection connection = (Connection)getItem(i);
+        Connection connection = (Connection) getItem(i);
 
-        TextView txtFrom = (TextView) view.findViewById(R.id.from);
-        txtFrom.setText(connection.getFrom().getStation().getName().trim());
+        if (connection.getSections().get(0).getJourney() != null) {
+            TextView txtTransport = (TextView) view.findViewById(R.id.product);
+            txtTransport.setText((String) connection.getSections().get(0).getJourney().getName());
+
+            TextView txtDirection = (TextView) view.findViewById(R.id.direction);
+            txtDirection.setText("Richtung " + (String) connection.getSections().get(0).getJourney().getTo());
+        }
 
         TextView txtTo = (TextView) view.findViewById(R.id.to);
-        txtTo.setText(connection.getTo().getStation().getName().trim());
+        txtTo.setText(connection.getTo().getStation().getName());
 
-        Date departure = new Date(connection.getFrom().getDepartureTimestamp().longValue()*1000);
+        TextView txtFrom = (TextView) view.findViewById(R.id.from);
+        txtFrom.setText(connection.getFrom().getStation().getName());
+
+
+        Date departure = new Date(connection.getFrom().getDepartureTimestamp().longValue() * 1000);
         TextView txtStart = (TextView) view.findViewById(R.id.start);
         txtStart.setText(tf.format(departure).trim());
 
-        Date arrival = new Date(Long.parseLong(connection.getTo().getArrivalTimestamp())*1000);
-        TextView txtEnd= (TextView) view.findViewById(R.id.end);
+        Date arrival = new Date(Long.parseLong(connection.getTo().getArrivalTimestamp()) * 1000);
+        TextView txtEnd = (TextView) view.findViewById(R.id.end);
         txtEnd.setText(tf.format(arrival).trim());
 
         TextView txtDuration = (TextView) view.findViewById(R.id.duration);
-        String duration = connection.getDuration().replace("00d","").trim();
-        duration= duration.substring(0, duration.length()-3);
+        String duration = connection.getDuration().replace("00d", "").trim();
+        duration = duration.substring(0, duration.length() - 3);
         txtDuration.setText(duration);
 
+        TextView txtPlattform = (TextView) view.findViewById(R.id.platform);
+        if (connection.getFrom().getPlatform() != null) {
+            txtPlattform.setText("Gl. " + (String) connection.getFrom().getPlatform());
+        }
+
+
+        ImageView imgCapaFirstClass = (ImageView) view.findViewById(R.id.class1);
+
+        Drawable drawCapaFristClass;
+
+
+        int capaFirst = 0;
+        if (connection.getCapacity1st() != null)
+            capaFirst = connection.getCapacity1st().intValue();
+
+        switch (capaFirst) {
+            case 1:
+                drawCapaFristClass = context.getResources().getDrawable(R.drawable.ic_capacity_low, null);
+                break;
+            case 2:
+                drawCapaFristClass = context.getResources().getDrawable(R.drawable.ic_capacity_medium, null);
+                break;
+            case 3:
+                drawCapaFristClass = context.getResources().getDrawable(R.drawable.ic_capacity_high, null);
+                break;
+            default:
+                drawCapaFristClass=null;
+                break;
+        }
+        imgCapaFirstClass.setImageDrawable(drawCapaFristClass);
+
+
+        ImageView imgCapaSecondClass = (ImageView) view.findViewById(R.id.class2);
+        Drawable drawCapaSecondClass;
+
+        int capaSecond = 0;
+        if (connection.getCapacity2nd() != null)
+            capaSecond = connection.getCapacity2nd().intValue();
+
+        switch (capaSecond) {
+            case 1:
+                drawCapaSecondClass = context.getResources().getDrawable(R.drawable.ic_capacity_low, null);
+                break;
+            case 2:
+                drawCapaSecondClass = context.getResources().getDrawable(R.drawable.ic_capacity_medium, null);
+                break;
+            case 3:
+                drawCapaSecondClass = context.getResources().getDrawable(R.drawable.ic_capacity_high, null);
+                break;
+            default:
+                drawCapaSecondClass=null;
+                break;
+        }
+        imgCapaSecondClass.setImageDrawable(drawCapaSecondClass);
+
+
+        ImageView imgProduct = (ImageView) view.findViewById(R.id.productIcon);
+
+        Drawable drawProduct;
+
+
+        String cat = "";
+        if (connection.getSections().get(0).getJourney() != null) {
+            cat = connection.getSections().get(0).getJourney().getCategoryCode();
+        }
+
+        switch (cat) {
+            case "4":
+                drawProduct = context.getResources().getDrawable(R.drawable.ic_boat, null);
+                break;
+            case "6":
+                drawProduct = context.getResources().getDrawable(R.drawable.ic_bus, null);
+                break;
+            case "3":
+                drawProduct = context.getResources().getDrawable(R.drawable.ic_train, null);
+                break;
+            default:
+                drawProduct = context.getResources().getDrawable(R.drawable.ic_train, null);
+                break;
+        }
+        imgProduct.setImageDrawable(drawProduct);
+
+        if(connection.getTo().getDelay()!=null) {
+            TextView txtDelay = (TextView) view.findViewById(R.id.delay);
+            txtDelay.setText("+" + connection.getTo().getDelay());
+        }
         return view;
     }
 
